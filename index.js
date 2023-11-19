9cec0039071ac4c71cfcb46bb342f96bf026b384
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2023-11-19 23:53:02 +0300

    #2 implement async done

--- index.js
+++ index.js
@@ -3,9 +3,16 @@
 const fs = require('fs');
 const { text } = require('stream/consumers');
 
+// ** This is the BLOCKING, synchronous way. 
 const textInput = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textInput);
-
 const textOutput = `This is what we know about the avocado : ${textInput}\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt',textOutput);
 console.log('File written!');
+
+// ** This is the NON-BLOCKING, asynchronous way.
+fs.readFile('./txt/start.txt','utf-8', (error,data) => {
+    console.log(data);
+});
+console.log('Will read file!');
+
