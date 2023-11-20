1496706c01b69244b6e788f192bb21ae552760f5
Volkan Karakus <64658867+volkaankarakus@users.noreply.github.com>
2023-11-20 12:19:53 +0300

    #8 implement nodemon

---
+++ packages.js
@@ -0,0 +1,26 @@
+// Two type of packages that we can install
+// 1 - simple(regular) dependencies
+// 2 - development dependencies
+
+ 
+// Slugify
+// npm install slugify
+//    which is the small tool that we can use  to make more readable URL's out of names.
+
+
+// NODEMON
+// for dev dependencies -> npm install nodemon --save-dev
+//  Nice tool that help us develop node.js applications by automatically restarting.
+//  whenever we change some files in our working directory.
+//  We want to run this regardless of this project -> sudo npm i nodemon --global 
+
+
+// in package.json we added "start" to "script"
+//  Now we can run this by using -> "npm run start" or "npm start"
+
+
+
+
+
+
+  
